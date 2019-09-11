a37899163fa3a9d9230b592d0484d9d8848e222d
Sam Moss <sam.moss321@gmail.com>
2019-09-11 14:45:40 -0400

    fixes bugs in JWT and gets things working

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,12 @@
 class JsonWebToken
   class << self
-    def encode(payload, exp = 24.hours_from_now)
-      payload.exp = exp.to_i
+    def encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
       JWT.encode(payload, Rails.application.secrets.secret_key_base)
     end
 
     def decode(token)
-      body = JWT.decode(token, Rails.application.secrets.secret_key_base[0])
+      body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
