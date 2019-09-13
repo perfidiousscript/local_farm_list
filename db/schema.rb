b98d9835e14dd382d394378ef2699fb162a8793b
Sam Moss <sam.moss321@gmail.com>
2019-09-13 10:03:49 -0400

    adds lat and long to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_133417) do
+ActiveRecord::Schema.define(version: 2019_09_13_135023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_09_11_133417) do
     t.string "email"
     t.string "password_digest"
     t.integer "user_type"
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
