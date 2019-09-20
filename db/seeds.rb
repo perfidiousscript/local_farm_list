9daf58a45ffcb7f2924a210ec1a9263b173f52b2
Sam Moss <sam.moss321@gmail.com>
2019-09-20 13:52:56 -0400

    adds user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(user_name: 'sam_moss', email:'thisis.fryou@gmail.com', user_type:'3')
