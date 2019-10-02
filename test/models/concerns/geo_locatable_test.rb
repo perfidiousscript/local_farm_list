1facc84a374ed21d5ad9aeae30aea76c5e5080c6
Sam Moss <sam.moss321@gmail.com>
2019-10-02 15:34:54 -0400

    adds fixtures and some tests of geo_locatable method

--- test/models/concerns/geo_locatable_test.rb
+++ test/models/concerns/geo_locatable_test.rb
@@ -14,4 +14,33 @@ class GeoLocatableTest < ActiveSupport::TestCase
     )
   end
 
+  def test_geo_search_returns_all_users
+    dummy = DummyClass.new
+
+    assert(
+      2,
+      dummy.geo_search(35.6, -35.6, 5, 'all').length,
+    )
+  end
+
+  def test_geo_search_returns_farmers
+    dummy = DummyClass.new
+
+    assert(
+      1,
+      dummy.geo_search(35.6, -35.6, 5, 'farmer').length,
+    )
+  end
+
+  def test_geo_search_returns_users
+    dummy = DummyClass.new
+
+    assert(
+      1,
+      dummy.geo_search(35.6, 35.6, 5, 'all').length,
+    )
+  end
+
+
+
 end
