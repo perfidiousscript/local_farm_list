041abe6572d2aa708b5f94a56ddbdfa4ea6b5a6a
Sam Moss <sam.moss321@gmail.com>
2019-10-02 15:47:19 -0400

    abstracts DummyClass creation

--- test/models/concerns/geo_locatable_test.rb
+++ test/models/concerns/geo_locatable_test.rb
@@ -5,39 +5,35 @@ class GeoLocatableTest < ActiveSupport::TestCase
     include GeoLocatable
   end
 
-  def test_geo_search_calls
-    dummy = DummyClass.new
+  def setup
+    @dummy = DummyClass.new
+  end
 
+  def test_geo_search_calls
     assert(
       0,
-      dummy.geo_search(0, 0, 5, 'all').length,
+      @dummy.geo_search(0, 0, 5, 'all').length,
     )
   end
 
   def test_geo_search_returns_all_users
-    dummy = DummyClass.new
-
     assert(
       2,
-      dummy.geo_search(35.6, -35.6, 5, 'all').length,
+      @dummy.geo_search(35.6, -35.6, 5, 'all').length,
     )
   end
 
   def test_geo_search_returns_farmers
-    dummy = DummyClass.new
-
     assert(
       1,
-      dummy.geo_search(35.6, -35.6, 5, 'farmer').length,
+      @dummy.geo_search(35.6, -35.6, 5, 'farmer').length,
     )
   end
 
   def test_geo_search_returns_users
-    dummy = DummyClass.new
-
     assert(
       1,
-      dummy.geo_search(35.6, 35.6, 5, 'all').length,
+      @dummy.geo_search(35.6, 35.6, 5, 'all').length,
     )
   end
 
