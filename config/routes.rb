d833376f28357342c3e28c5a7a881e6cfe65d5f6
Sam Moss <sam.moss321@gmail.com>
2019-10-03 18:48:56 -0400

    singularizes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
-  resources :users
-  resources :farms
+  resources :user, :farm
+
+  get 'farms/search_nearby(/:distance)', to: 'farms#search_nearby'
 end
